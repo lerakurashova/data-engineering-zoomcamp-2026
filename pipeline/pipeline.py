bf1f0235bc859a8b25e6b80e98950db82d89c0b7
lerakurashova <kurashova.lera@gmail.com>
2026-01-09 02:46:56 +0000

    Refactor code structure for improved readability and maintainability

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,10 +1,17 @@
 import sys 
 import pandas as pd
 
-df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
-print(df.head())
-
 #parameter for a pipeline 
 month = int(sys.argv[1])
+
+print(f"Processing data for month: {month}")
+
+df = pd.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
+#create a new column based on the month parameter
+df['month'] = month
+print(df.head())
+#save the dataframe to a parquet file
+df.to_parquet(f"data_passengers_month_{month}.parquet", index=False)
+
 
 
